3d7d13dbdbced56c9b8d6a64adaf9118d76b2f0b
Fahrettin <u.f.gungor@hotmail.com>
2018-05-18 15:27:26 +0200

    Turn cursor off
    -SplashScreen script now turns the Cursor off
    - In each scene new script added, that handes the eventsystem

---
+++ RingOutTheGame/Assets/_Scripts/Code_DisableCursor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.EventSystems;
+
+public class Code_DisableCursor : StandaloneInputModule {
+    public override void Process() {
+        bool usedEvent = SendUpdateEventToSelectedObject();
+
+        if (eventSystem.sendNavigationEvents) {
+            if (!usedEvent)
+                usedEvent |= SendMoveEventToSelectedObject();
+
+            if (!usedEvent)
+                SendSubmitEventToSelectedObject();
+        }
+    }
+}
